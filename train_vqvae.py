ff7b1514098d3baccc41ccf1663c98f885abbbcd
RongDai430 <dairong430@gmail.com>
2021-09-20 14:48:42 +0800

    9.20

---
+++ train_vqvae.py
@@ -0,0 +1,205 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from tqdm import tqdm
+from copy import deepcopy
+import torchvision
+import torchvision.transforms as transforms
+import wandb
+import os
+import time
+import argparse
+import datetime
+from torch.autograd import Variable
+import pdb
+import sys
+import torch.autograd as autograd
+import torchvision.models as models
+sys.path.append('.')
+
+from vae import *
+from set import *
+from load_imagenet import imagenet, load_data
+
+
+def reconst_images(batch_size=64, batch_num=1, dataloader=None, model=None):
+    cifar10_dataloader = dataloader
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (X, y) in enumerate(cifar10_dataloader):
+            if batch_idx >= batch_num:
+                break
+            else:
+                X, y = X.cuda(), y.cuda().view(-1, )
+                gx, z_e, emb = model(X)
+
+                grid_X = torchvision.utils.make_grid(X[:batch_size].data, nrow=8, padding=2, normalize=True)
+                wandb.log({"_Batch_{batch}_X.jpg".format(batch=batch_idx): [
+                    wandb.Image(grid_X)]}, commit=False)
+                grid_Xi = torchvision.utils.make_grid(gx[:batch_size].data, nrow=8, padding=2, normalize=True)
+                wandb.log({"_Batch_{batch}_GX.jpg".format(batch=batch_idx): [
+                    wandb.Image(grid_Xi)]}, commit=False)
+                grid_X_Xi = torchvision.utils.make_grid((X[:batch_size] - gx[:batch_size]).data, nrow=8, padding=2,
+                                                        normalize=True)
+                wandb.log({"_Batch_{batch}_RX.jpg".format(batch=batch_idx): [
+                    wandb.Image(grid_X_Xi)]}, commit=False)
+    print('reconstruction complete!')
+
+
+def test(epoch, model, testloader):
+    # set model as testing mode
+    model.eval()
+    acc_gx_avg = AverageMeter()
+    acc_rx_avg = AverageMeter()
+
+    with torch.no_grad():
+        for batch_idx, (x, y) in enumerate(testloader):
+            # distribute data to device
+            x, y = x.cuda(), y.cuda().view(-1, )
+            bs = x.size(0)
+            norm = torch.norm(torch.abs(x.view(bs, -1)), p=2, dim=1)
+            gx, z_e, emb = model(x)
+            acc_gx = 1 - F.mse_loss(torch.div(gx, norm.unsqueeze(1).unsqueeze(2).unsqueeze(3)), \
+                                    torch.div(x, norm.unsqueeze(1).unsqueeze(2).unsqueeze(3)), \
+                                    reduction='sum') / bs
+            acc_rx = 1 - F.mse_loss(torch.div(x - gx, norm.unsqueeze(1).unsqueeze(2).unsqueeze(3)), \
+                                    torch.div(x, norm.unsqueeze(1).unsqueeze(2).unsqueeze(3)), \
+                                    reduction='sum') / bs
+
+            acc_gx_avg.update(acc_gx.data.item(), bs)
+            acc_rx_avg.update(acc_rx.data.item(), bs)
+
+        wandb.log({'acc_gx_avg': acc_gx_avg.avg, \
+                   'acc_rx_avg': acc_rx_avg.avg}, commit=False)
+        # plot progress
+        print("\n| Validation Epoch #%d\t\tRec_gx: %.4f Rec_rx: %.4f " % (epoch, acc_gx_avg.avg, acc_rx_avg.avg))
+        reconst_images(batch_size=64, batch_num=2, dataloader=testloader, model=model)
+        torch.save(model.module.state_dict(),
+                   os.path.join(args.save_dir, 'model_epoch{}.pth'.format(epoch + 1)))  # save motion_encoder
+        print("Epoch {} model saved!".format(epoch + 1))
+
+
+def main(args):
+    setup_logger(args.save_dir)
+    use_cuda = torch.cuda.is_available()
+    print('\n[Phase 1] : Data Preparation')
+
+
+    size = 224
+    normalizer = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    model = VQVAE_imagenet(d=args.dim, k=args.kdim)
+
+    if args.simclr:
+        s = 1
+        color_jitter = transforms.ColorJitter(
+            0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s
+        )
+        transform_train = transforms.Compose(
+            [
+            transforms.RandomResizedCrop(size=size),
+            transforms.RandomHorizontalFlip(),  # with 0.5 probability
+            transforms.RandomApply([color_jitter], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ToTensor(),
+            normalizer
+            ]
+        )
+    else:
+        transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(size=size, scale=(0.2, 1.)),
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalizer
+        ])
+
+    print("| Preparing Tiny-Imagenet dataset...")
+    sys.stdout.write("| ")
+    trainset, _ = load_data('../data/tiny_imagenet.pickle')
+    trainset = imagenet(trainset, transform=transform_train)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=True)
+
+    # Model
+    print('\n[Phase 2] : Model setup')
+    if use_cuda:
+        model.cuda()
+        model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+        cudnn.benchmark = True
+
+    optimizer = AdamW([
+        {'params': model.parameters()},
+    ], lr=args.lr, betas=(0.0, 0.9))
+
+    scheduler = optim.lr_scheduler.LambdaLR(
+        optimizer, lambda epoch: 1 - epoch / args.epochs)
+
+    print('\n[Phase 3] : Training model')
+    print('| Training Epochs = ' + str(args.epochs))
+    print('| Initial Learning Rate = ' + str(args.lr))
+
+    start_epoch = 1
+    for epoch in range(start_epoch, start_epoch + args.epochs):
+        model.train()
+
+        loss_avg = AverageMeter()
+        loss_rec = AverageMeter()
+        loss_kl = AverageMeter()
+
+        print('\n=> Training Epoch #%d, LR=%.4f' % (epoch, optimizer.param_groups[0]['lr']))
+        for batch_idx, (x, y) in enumerate(trainloader):
+            x, y = x.cuda(), y.cuda().view(-1, )
+            x, y = Variable(x), Variable(y)
+            bs = x.size(0)
+
+            gx, z_e, emb = model(x)
+
+            optimizer.zero_grad()
+            l_rec = F.mse_loss(x, gx)
+            l_kl = torch.mean(torch.norm((emb - z_e.detach())**2, 2, 1)) + 0.5*torch.mean(torch.norm((emb.detach() - z_e)**2, 2, 1))
+            loss = l_rec + args.kl * l_kl
+            loss.backward()
+            optimizer.step()
+
+            loss_avg.update(loss.data.item(), bs)
+            loss_rec.update(l_rec.data.item(), bs)
+            loss_kl.update(l_kl.data.item(), bs)
+
+            n_iter = (epoch - 1) * len(trainloader) + batch_idx
+            wandb.log({'loss': loss_avg.avg, \
+                       'loss_rec': loss_rec.avg, \
+                       'loss_kl': loss_kl.avg, \
+                       'lr': optimizer.param_groups[0]['lr']}, step=n_iter)
+            if (batch_idx + 1) % 30 == 0:
+                sys.stdout.write('\r')
+                sys.stdout.write(
+                    '| Epoch [%3d/%3d] Iter[%3d/%3d]\t\t Loss_rec: %.4f Loss_kl: %.4f'
+                    % (epoch, args.epochs, batch_idx + 1,
+                       len(trainloader),  loss_rec.avg, loss_kl.avg))
+        scheduler.step()
+        if epoch % 10 == 1:
+            test(epoch, model, trainloader)
+    wandb.finish()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
+    parser.add_argument('--lr', default=5e-4, type=float, help='learning_rate')
+    parser.add_argument('--save_dir', default='./results/autoaug_new_8_0.5', type=str, help='save_dir')
+    parser.add_argument('--seed', default=666, type=int, help='seed')
+    parser.add_argument('--dataset', default='cifar10', type=str, help='dataset = [cifar10/cifar100]')
+    parser.add_argument('--epochs', default=300, type=int, help='training_epochs')
+    parser.add_argument('--batch_size', default=128, type=int, help='batch_size')
+    parser.add_argument('--dim', default=128, type=int, help='CNN_embed_dim')
+    parser.add_argument('--kdim', default=128, type=int, help='CNN_embed_dim')
+    parser.add_argument('--kl', default=1, type=float, help='kl weight')
+    parser.add_argument('--simclr', default=False, type=str, help='simclr')
+    args = parser.parse_args()
+    wandb.init(config=args, name=args.save_dir.replace("./results/", ''))
+    set_random_seed(args.seed)
+    main(args)
--- vae.py
+++ vae.py
@@ -12,6 +12,8 @@ from torch.nn import init
 from torch.nn import functional as F
 from torch.autograd import Variable
 import torchvision.models as models
+import torch.nn.functional as F
+from torch.autograd import Function, Variable
 
 
 class ResBlock(nn.Module):
@@ -218,3 +220,143 @@ class CVAE_imagenet_withbn(AbstractAutoEncoder):
             gx = self.decode(z_projected)
             gx = self.bn(gx)
         return z, gx, mu, logvar
+
+
+class VQVAE_imagenet(nn.Module):
+    def __init__(self, d, k=10, num_channels=3, **kwargs):
+        super(VQVAE_imagenet, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(num_channels, d//2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d//2),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(d//2, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(d, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.LeakyReLU(inplace=True),
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+        )
+        self.decoder = nn.Sequential(
+            ResBlock(d, d),
+            nn.BatchNorm2d(d),
+            ResBlock(d, d),
+            nn.ConvTranspose2d(d, d, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d),
+            nn.LeakyReLU(inplace=True),
+            nn.ConvTranspose2d(d, d//2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(d//2),
+            nn.LeakyReLU(inplace=True),
+            nn.ConvTranspose2d(d//2, num_channels, kernel_size=4, stride=2, padding=1),
+        )
+        self.d = d
+        self.emb = NearestEmbed(k, d)
+
+        for l in self.modules():
+            if isinstance(l, nn.Linear) or isinstance(l, nn.Conv2d):
+                l.weight.detach().normal_(0, 0.02)
+                torch.fmod(l.weight, 0.04)
+                nn.init.constant_(l.bias, 0)
+
+        self.encoder[-1].weight.detach().fill_(1 / 40)
+
+        self.emb.weight.detach().normal_(0, 0.02)
+        torch.fmod(self.emb.weight, 0.04)
+        self.L_bn = nn.BatchNorm2d(num_channels)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return torch.tanh(self.decoder(x))
+
+    def forward(self, x):
+        z_e = self.encode(x)
+
+        z_q, _ = self.emb(z_e, weight_sg=True)
+        emb, _ = self.emb(z_e.detach())
+
+        l = self.decode(z_q)
+        xi = self.L_bn(l)
+
+        return xi, z_e, emb
+
+
+class NearestEmbedFunc(Function):
+    """
+    Input:
+    ------
+    x - (batch_size, emb_dim, *)
+        Last dimensions may be arbitrary
+    emb - (emb_dim, num_emb)
+    """
+    @staticmethod
+    def forward(ctx, input, emb):
+        if input.size(1) != emb.size(0):
+            raise RuntimeError('invalid argument: input.size(1) ({}) must be equal to emb.size(0) ({})'.
+                               format(input.size(1), emb.size(0)))
+
+        # save sizes for backward
+        ctx.batch_size = input.size(0)
+        ctx.num_latents = int(np.prod(np.array(input.size()[2:])))
+        ctx.emb_dim = emb.size(0)
+        ctx.num_emb = emb.size(1)
+        ctx.input_type = type(input)
+        ctx.dims = list(range(len(input.size())))
+
+        # expand to be broadcast-able
+        x_expanded = input.unsqueeze(-1)
+        num_arbitrary_dims = len(ctx.dims) - 2
+        if num_arbitrary_dims:
+            emb_expanded = emb.view(emb.shape[0], *([1] * num_arbitrary_dims), emb.shape[1])
+        else:
+            emb_expanded = emb
+
+        # find nearest neighbors
+        dist = torch.norm(x_expanded - emb_expanded, 2, 1)
+        _, argmin = dist.min(-1)
+        shifted_shape = [input.shape[0], *list(input.shape[2:]) ,input.shape[1]]
+        result = emb.t().index_select(0, argmin.view(-1)).view(shifted_shape).permute(0, ctx.dims[-1], *ctx.dims[1:-1])
+
+        ctx.save_for_backward(argmin)
+        return result.contiguous(), argmin
+
+    @staticmethod
+    def backward(ctx, grad_output, argmin=None):
+        grad_input = grad_emb = None
+        if ctx.needs_input_grad[0]:
+            grad_input = grad_output
+
+        if ctx.needs_input_grad[1]:
+            argmin, = ctx.saved_variables
+            latent_indices = torch.arange(ctx.num_emb).type_as(argmin)
+            idx_choices = (argmin.view(-1, 1) == latent_indices.view(1, -1)).type_as(grad_output.data)
+            n_idx_choice = idx_choices.sum(0)
+            n_idx_choice[n_idx_choice == 0] = 1
+            idx_avg_choices = idx_choices / n_idx_choice
+            grad_output = grad_output.permute(0, *ctx.dims[2:], 1).contiguous()
+            grad_output = grad_output.view(ctx.batch_size * ctx.num_latents, ctx.emb_dim)
+            grad_emb = torch.sum(grad_output.data.view(-1, ctx.emb_dim, 1) *
+                                 idx_avg_choices.view(-1, 1, ctx.num_emb), 0)
+        return grad_input, grad_emb, None, None
+
+
+def nearest_embed(x, emb):
+    return NearestEmbedFunc().apply(x, emb)
+
+
+class NearestEmbed(nn.Module):
+    def __init__(self, num_embeddings, embeddings_dim):
+        super(NearestEmbed, self).__init__()
+        self.weight = nn.Parameter(torch.rand(embeddings_dim, num_embeddings))
+
+    def forward(self, x, weight_sg=False):
+        """Input:
+        ---------
+        x - (batch_size, emb_size, *)
+        """
+        return nearest_embed(x, self.weight.detach() if weight_sg else self.weight)
